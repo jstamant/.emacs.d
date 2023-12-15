db21efecf5e0c69a3704add893a62719f436d0b2
Justin R. St-Amant <jstamant24@gmail.com>
2023-12-15 12:29:50 -0600

    Cleaned-up modeline config

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -1,18 +1,17 @@
-;;; init-modeline.el --- Modeline setup and configuration -*- lexical-binding: t -*-
+;;; init-modeline.el --- Modeline setup and configuration with doom-modeline -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
 
-;; TODO implement doom-modeline (this is what spacemacs uses on graphical displays
-;; and implement either sml or vim-powerline for non-graphical displays
-;; TODO idk...I kinda preferred smart-mode-line.
-;; I don't like how all the mode lighters are missing.
 (use-package doom-modeline
-  :after nerd-icons
   :ensure t
+  :hook (after-init . doom-modeline-mode)
   :config
-  (doom-modeline-mode 1)
-  (setq doom-modeline-height 15))
+  (setq doom-modeline-height 25)
+  (setq doom-modeline-icon t)
+  (setq doom-modeline-modal t
+        doom-modeline-modal-icon t
+        doom-modeline-modal-modern-icon t))
 
 (use-package nerd-icons
   :ensure t)
