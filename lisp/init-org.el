94b0616b7ac3fc88a87dfe4837317c99740ec7eb
Justin R. St-Amant <jstamant24@gmail.com>
2024-03-08 13:19:29 -0500

    Added org-roam and beginner settings

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,4 +1,4 @@
-;;; init-org.el --- Personal org-mode configuration -*- lexical-binding: t -*-
+;;; init-org.el --- Personal org-mode and org-roam configuration -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
@@ -119,9 +119,9 @@ This command is usually invoked as an accident.\n")
 
 (defun jstamant/init-org-capture ()
   "Set all my org-capture settings."
-  ;; TODO move these to evil-mode bindings
-  (global-set-key "\C-cc" 'org-capture)
-  (global-set-key "\C-cl" 'org-store-link)
+  ;; TODO move these bindings to general.el
+  ;; (global-set-key "\C-cc" 'org-capture)
+  ;; (global-set-key "\C-cl" 'org-store-link)
   (setq org-capture-templates
         `(("t" "TODO" entry
            (file+headline ,(car org-agenda-files) "Tasks")
@@ -138,51 +138,20 @@ This command is usually invoked as an accident.\n")
           ("j" "Journal entry - plain" plain
            (file+datetree "reference/journal.org") ""))))
 
-;; (defun dw/org-mode-setup ()
-;;   (org-indent-mode)
-;;   (variable-pitch-mode 1)
-;;   (auto-fill-mode 0)
-;;   (visual-line-mode 1)
-;;   (setq evil-auto-indent nil))
 
-;; (use-package org
-;;   :hook (org-mode . dw/org-mode-setup)
-;;   :config
-;;   (setq org-ellipsis " ▾"
-;;         org-hide-emphasis-markers t))
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-directory (concat drive-directory "/org-roam"))
+  :config
+  (org-roam-db-autosync-mode))
 
-;; (use-package org-bullets
-;;   :after org
-;;   :hook (org-mode . org-bullets-mode)
-;;   :custom
-;;   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-;; ;; Replace list hyphen with dot
-;; (font-lock-add-keywords 'org-mode
-;;                         '(("^ *\\([-]\\) "
-;;                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-;; (dolist (face '((org-level-1 . 1.2)
-;;                 (org-level-2 . 1.1)
-;;                 (org-level-3 . 1.05)
-;;                 (org-level-4 . 1.0)
-;;                 (org-level-5 . 1.1)
-;;                 (org-level-6 . 1.1)
-;;                 (org-level-7 . 1.1)
-;;                 (org-level-8 . 1.1)))
-;;     (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
-
-;; ;; Make sure org-indent face is available
-;; (require 'org-indent)
-
-;; ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-;; (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-;; (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-;; (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
-;; (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-;; (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-;; (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-;; (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+(jrs/emacs-leader-keys
+  "n" '(:ignore t :which-key "org-roam node")
+  "nc" 'org-roam-node-capture
+  "nf" 'org-roam-node-find
+  "ni" 'org-roam-node-insert
+  "nl" '(org-roam-buffer-toggle :which-key "links"))
 
 
 (provide 'init-org)
