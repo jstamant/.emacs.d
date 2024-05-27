5e33f441f66e1e6b52c8ca0a3940fea7ff9c8fe6
Justin St-Amant <jstamant24@gmail.com>
2024-05-27 16:10:27 -0500

    Auto updating and installing tree-sitter grammars

--- lisp/init-tree-sitter.el
+++ lisp/init-tree-sitter.el
@@ -5,16 +5,31 @@
 
 (use-package tree-sitter
   :ensure t
+  :diminish tree-sitter-mode
   :config
   (global-tree-sitter-mode)
   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
-;; TODO grammars need to be moved to the tree-sitter folder after they're compiled
-;; TODO figure out deferring of tree-sitter-langs?
 (use-package tree-sitter-langs
   :ensure t
-  :diminish tree-sitter-mode)
-  ;; :after tree-sitter)
+  :config
+  ;; Automatically update and install tree-sitter grammars
+  (when (not (file-exists-p (concat user-emacs-directory "tree-sitter/BUNDLE-VERSION")))
+    (tree-sitter-langs-install-latest-grammar
+     'skip-if-installed
+     tree-sitter-langs--os)
+    (let ((tree-sitter-dir (concat user-emacs-directory "tree-sitter/"))
+          (grammar-files (directory-files (concat tree-sitter-langs--dir "bin") nil ".*\\.[a-z]")))
+      (mapc (lambda (file)
+              (let ((new-file-name (concat "libtree-sitter-" file)))
+                (copy-file (concat tree-sitter-langs--dir "bin/" file)
+                           (concat tree-sitter-dir new-file-name)
+                           'override-files)))
+            grammar-files)
+      (copy-file (concat tree-sitter-langs--dir "bin/BUNDLE-VERSION")
+                 (concat tree-sitter-dir)
+                 'override-files)
+      (concat "Copied grammar files to " tree-sitter-dir))))
 
 
 (provide 'init-tree-sitter)
