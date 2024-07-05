ff546873e04197fb4e36c73265ab2f2023a8a001
Justin St-Amant <jstamant24@gmail.com>
2024-07-04 21:56:22 -0500

    Added basic org theming

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -33,6 +33,13 @@
   (setq modus-themes-mixed-fonts t)
   ;; (setq modus-vivendi-palette-overrides
   ;;       '((bg-main "#222222")))
+  (setq modus-themes-headings
+        '((0 . (variable-pitch 1.8))
+          (1 . (1.5))
+          (2 . (1.3))
+          (agenda-date . (1.3))
+          (agenda-structure . (variable-pitch light 1.8))
+          (t . (1.1))))
   (load-theme 'modus-vivendi))
 
 
