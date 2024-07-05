a88bae9f8436f43cf97e50e767e310df6d34a720
Justin St-Amant <jstamant24@gmail.com>
2024-07-05 15:41:44 -0500

    Changed heading styling

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -33,10 +33,14 @@
   (setq modus-themes-mixed-fonts t)
   ;; (setq modus-vivendi-palette-overrides
   ;;       '((bg-main "#222222")))
+  (setq modus-themes-common-palette-overrides
+        '((fg-heading-1 blue-warmer)
+          (fg-heading-3 fg-alt)))
   (setq modus-themes-headings
         '((0 . (variable-pitch 1.8))
           (1 . (1.5))
           (2 . (1.3))
+          (3 . (1.2))
           (agenda-date . (1.3))
           (agenda-structure . (variable-pitch light 1.8))
           (t . (1.1))))
