5b684825d0d299c0844f717c2287fe317232d231
Justin St-Amant <jstamant24@gmail.com>
2025-09-19 20:34:33 -0300

    Fixed kill-buffer keybinding since Emacs 30

--- lisp/init-buffers.el
+++ lisp/init-buffers.el
@@ -2,19 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
+;; TODO remove this?
+;; (jrs/emacs-leader-keys
+;;   "b" '(:ignore t :which-key "buffers")
+;;   "bb" 'switch-to-buffer
+;;   "bl" 'list-buffers
+;;   "bd" 'kill-this-buffer ; TODO has to be changed to kill-current-buffer
+;;   "bk" 'kill-this-buffer)
 
-(jrs/emacs-leader-keys
-  "b" '(:ignore t :which-key "buffers")
-  "bb" 'switch-to-buffer
-  "bl" 'list-buffers
-  "bd" 'kill-this-buffer
-  "bk" 'kill-this-buffer)
+(keymap-set ctl-x-map "k" 'kill-current-buffer)
+(keymap-set ctl-x-map "K" 'kill-buffer)
 
-(keymap-set ctl-x-map "k" 'kill-this-buffer)
-
-(general-spc
-  "b" 'switch-to-buffer
-  "k" 'kill-this-buffer)
 
 (provide 'init-buffers)
 ;;; init-buffers.el ends here
