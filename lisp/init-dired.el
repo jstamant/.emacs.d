1f6bb91dd5de878f80627d1982c7255d8d727507
Justin St-Amant <jstamant24@gmail.com>
2025-12-23 12:13:59 -0400

    Add keybind for dired-up-directory

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -6,7 +6,8 @@
 (with-eval-after-load 'dired
   (put 'dired-find-alternate-file 'disabled nil)
   (keymap-set dired-mode-map "C-s" 'dired-isearch-filenames)
-  (keymap-set dired-mode-map "TAB" 'dired-find-file))
+  (keymap-set dired-mode-map "TAB" 'dired-find-file)
+  (keymap-set dired-mode-map "b" 'dired-up-directory))
 
 ;; Uses ls-lisp to prevent errors on Mac
 (when (eq system-type 'darwin)
